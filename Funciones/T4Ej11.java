dee9005ee3d9a72ee87f26a044bb3df37cd5903b
Adrian Muro jimenez <59072122+AdriiPlays@users.noreply.github.com>
2024-10-30 21:22:33 +0100

    Add files via upload

--- Funciones/T4Ej11.java
+++ Funciones/T4Ej11.java
@@ -3,6 +3,7 @@ package Tema4;
 public class T4Ej11 {
 	public static void main(String[] args) {
 		sonSimilares(144, 10);
+		System.out.println(sonSimilares2(1234, 3));
 
 	}
 
@@ -21,4 +22,41 @@ public class T4Ej11 {
 
 	}
 
+	public static boolean contieneDigito(int n1, int digito) {
+		while (n1 != 0) {
+			if (n1 % 10 == digito) {
+				return true;
+
+			}
+			n1 = n1 / 10;
+
+		}
+		return false;
+
+	}
+
+	public static boolean sonSimilares2(int n1, int n2) {
+		
+int aux;
+if (n2>n1) {
+	aux= n1;
+	n1 = n2;
+	n2=aux;
+}
+	
+
+		while (n2 != 0) {
+			if (contieneDigito(n1, n2%10) == false) {
+				
+			n2 = n2/10;
+			return true;
+			
+			
+		}
+
+		return true;
+
+	}
+		return false;
+	}
 }
